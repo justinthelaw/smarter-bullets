4ca0078b66b133453d066f84785e6aa083de45ef
Alden Davidson <gitlab.washout767@simplelogin.com>
2023-07-12 22:28:43 -0700

    add input textbox w/ output textbox shadowing it

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { FormControl, FormLabel, Textarea } from '@mui/joy'
+import React, { useState } from 'react'
 
 const Home: React.FC = () => {
+  const [bullets, setBullets] = useState<string>()
+
   return (
     <div data-testid='home.page'>
       <h1>Smarter Bullets</h1>
+      <FormControl>
+        <FormLabel>Input Bullets Here:</FormLabel>
+        <Textarea minRows={2} value={bullets} onChange={(e) => setBullets(e.target.value)} />
+      </FormControl>
+      <FormControl>
+        <FormLabel>View Output Here:</FormLabel>
+        <Textarea minRows={2} value={bullets} />
+      </FormControl>
     </div>
   )
 }
--- client/tests/pages/Home.test.tsx
+++ client/tests/pages/Home.test.tsx
@@ -1,11 +1,39 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
-import { render } from '@testing-library/react'
 import Home from '../../src/pages/Home'
 
-describe('App', () => {
-	test('should render application title, smarter bullets', () => {
-		const { getByText } = render(<Home />)
-		const appTitleElement = getByText(/smarter bullets/i)
-		expect(appTitleElement).toBeInTheDocument()
-	})
+describe('Home', () => {
+  it('should render application title, smarter bullets', () => {
+    setup()
+
+    const appTitle = screen.getByText(/smarter bullets/i)
+    expect(appTitle).toBeVisible()
+  })
+
+  it('should display a textbox that can be typed in to input bullets', async () => {
+    const { user } = setup()
+
+    const bulletInputTextbox = screen.getByRole('textbox', { name: /Input Bullets Here:/i })
+
+    const bulletText = 'this is a test bullet'
+    await user.type(bulletInputTextbox, bulletText)
+
+    expect(bulletInputTextbox).toBeVisible()
+    expect(bulletInputTextbox).toHaveTextContent(bulletText)
+  })
+
+  it('should display a textbox that displays formatted bullets', () => {
+    setup()
+
+    const bulletOutputTextbox = screen.getByRole('textbox', { name: /View Output Here:/i })
+    expect(bulletOutputTextbox).toBeVisible()
+  })
 })
+
+const setup = () => {
+  return {
+    user: userEvent.setup(),
+    ...render(<Home />)
+  }
+}
